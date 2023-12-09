800cecee89951e44b8e46c2312c0b4280a4dcc71
Araar-Taha <araartaha@gmail.com>
2023-12-09 23:23:24 +0100

    created the user model we need

--- core/authy/models.py
+++ core/authy/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('User', 'User'),
+        ('Mod', 'Moderator'),
+        ('Admin', 'Admin'),
+    ]
+    #an AbstractUser django class have already the fields that we need
+    #we just need to add the user-type attribute
+    user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='User',)
+    def __str__(self):
+        return f"{self.username} ({self.user_type})"
