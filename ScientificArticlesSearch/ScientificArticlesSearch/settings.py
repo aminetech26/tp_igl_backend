2c893b3b9e911659db0bad066ad306a7d554cf21
Benmachiche Khaled <lk_benmachiche@esi.dz>
2023-12-22 01:40:33 +0100

    neon db

--- ScientificArticlesSearch/ScientificArticlesSearch/settings.py
+++ ScientificArticlesSearch/ScientificArticlesSearch/settings.py
@@ -99,10 +99,15 @@ WSGI_APPLICATION = 'ScientificArticlesSearch.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT'),
+    'OPTIONS': {'sslmode': 'require'},
+  }
 }
 
 
