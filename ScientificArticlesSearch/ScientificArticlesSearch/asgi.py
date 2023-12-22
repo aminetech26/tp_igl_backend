b1b873417ef473eaf453b006fa9a8387d90719fe
Benmachiche Khaled <lk_benmachiche@esi.dz>
2023-12-22 01:19:01 +0100

    Merge changes from feature/article_upload

--- ScientificArticlesSearch/ScientificArticlesSearch/asgi.py
+++ ScientificArticlesSearch/ScientificArticlesSearch/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:ScientificArticlesSearch/ScientificArticlesSearch/asgi.py
 """
 ASGI config for ScientificArticlesSearch project.
 
@@ -15,21 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ScientificArticlesSearch.settings')
 
 application = get_asgi_application()
-========
-"""
-ASGI config for api project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
-application = get_asgi_application()
->>>>>>>> feature/article_upload:api/api/asgi.py
--- ScientificArticlesSearch/ScientificArticlesSearch/wsgi.py
+++ ScientificArticlesSearch/ScientificArticlesSearch/wsgi.py
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:ScientificArticlesSearch/ScientificArticlesSearch/wsgi.py
 """
 WSGI config for ScientificArticlesSearch project.
 
@@ -15,21 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ScientificArticlesSearch.settings')
 
 application = get_wsgi_application()
-========
-"""
-WSGI config for api project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-
-application = get_wsgi_application()
->>>>>>>> feature/article_upload:api/api/wsgi.py
