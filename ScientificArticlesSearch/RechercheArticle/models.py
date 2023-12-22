4d0ed3e95287f0958c80686b597d6aae5b2d7a6a
Benmachiche Khaled <lk_benmachiche@esi.dz>
2023-12-22 02:18:48 +0100

    updated models

--- ScientificArticlesSearch/RechercheArticle/models.py
+++ ScientificArticlesSearch/RechercheArticle/models.py
@@ -1,10 +1,3 @@
 from django.db import models
-from Articles.models import Article
 
 
-
-class ArticleFavoris(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
