a69c5f4646f760e30f483fa577c206b10313d66f
Benmachiche Khaled <lk_benmachiche@esi.dz>
2023-12-22 12:46:07 +0100

    .env file

--- ScientificArticlesSearch/ScientificArticlesSearch/settings.py
+++ ScientificArticlesSearch/ScientificArticlesSearch/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,8 +106,10 @@ DATABASES = {
     'HOST': os.getenv('DB_HOST'),
     'PORT': os.getenv('DB_PORT'),
     'OPTIONS': {'sslmode': 'require'},
+    'DISABLE_SERVER_SIDE_CURSORS': True,
   }
 }
+
 
 
 # Password validation
