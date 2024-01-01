0a84c88de1b9243a2ff4349c477e912f4a853d2f
Benmachiche Khaled <lk_benmachiche@esi.dz>
2024-01-01 01:19:07 +0100

    auth in one ModelviewSet class for delete,update,get user pre-implemented

--- ScientificArticlesSearch/Users/urls.py
+++ ScientificArticlesSearch/Users/urls.py
@@ -1,13 +1,11 @@
-from django.contrib import admin
-from django.urls import path
-from .views import RegisterView , LoginView , LogoutView , UserView , RefreshView
+from django.urls import path, include
+from rest_framework import routers
+
+from .views import UserViewSet
+
+router = routers.DefaultRouter()
+router.register(r"", UserViewSet)
 
 urlpatterns = [
-    path('register', RegisterView.as_view()),
-    path('login', LoginView.as_view()),
-    path('logout', LogoutView.as_view()),
-    path('refreshView', RefreshView.as_view()),
-#for testing
-    path('userView', UserView.as_view()),
-
+    path("", include(router.urls)),
 ]
--- ScientificArticlesSearch/Users/views.py
+++ ScientificArticlesSearch/Users/views.py
@@ -1,22 +1,24 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
 from .models import User
-import jwt, datetime
+import jwt
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from django.conf import settings
+from .utils import create_token, decode_token
+
+TOKEN_EXPIRATION_ACCESS = 10
+TOKEN_EXPIRATION_REFRESH = 1440
 
 
-# Create your views here.
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-
-class LoginView(APIView):
-    def post(self, request):
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    # Model View Set already has the get() and get_by_id() create() and update() and delete() methods implemented
+    
+    @action(detail=False, methods=['post'])
+    def login(self, request):
         username = request.data['username']
         password = request.data['password']
 
@@ -27,86 +29,47 @@ class LoginView(APIView):
 
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
-
+        
         payload = {
             'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=1440),
-            'iat': datetime.datetime.utcnow()
         }
-        #refresh token will last for 24 hours
-        RefreshToken = jwt.encode(payload, 'a-very-seCreT-kEy-WOW', algorithm='HS256')
-
-        #access token
-        #to test refresh i am putting the expiration time of the access token to 
-        payload2 = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10),
-            'iat': datetime.datetime.utcnow()
-        }       
-        AccessToken = jwt.encode(payload2, 'a-very-seCreT-kEy-WOW', algorithm='HS256')
+        refresh_token = create_token(payload, TOKEN_EXPIRATION_REFRESH, settings.SECRET_KEY)
+        access_token = create_token(payload, TOKEN_EXPIRATION_ACCESS, settings.SECRET_KEY)
+        
         response = Response()
 
-        response.set_cookie(key='RefreshToken', value=RefreshToken, httponly=True)
+        response.set_cookie(key='RefreshToken', value=refresh_token, httponly=True)
         #user type is also returned in the response along with the access token
         response.data = {
-            'AccessToken': AccessToken,
+            'AccessToken': access_token,
             'user-type' : user.user_type
         }
         return response
-
-
-
-class UserView(APIView):
-
-    def get(self, request):
-        # token = request.COOKIES.get('AccessToken')
-        #put the access token on a request  
-        print('user view fired')
-        token = request.headers.get('AccessToken')
-        print(token)
-        if not token:
-            raise AuthenticationFailed('Unauthenticated!')
-
-        try:
-            payload = jwt.decode(token, 'a-very-seCreT-kEy-WOW', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated!')
-
-        user = User.objects.filter(id=payload['id']).first()
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
     
-class LogoutView(APIView):
-    def post(self, request):
+    @action(detail=False, methods=['post'])
+    def logout(self, request):
         response = Response()
         response.delete_cookie('RefreshToken')
         response.data = {
             'message': 'success'
         }
-        return response  
-
-class RefreshView(APIView):
-    def post(self, request):
-        refreshToken = request.COOKIES.get('RefreshToken')
-        if not refreshToken:
+        return response
+    
+    @action(detail=False, methods=['post'])
+    def refresh(self,request):
+        refresh_token = request.COOKIES.get('RefreshToken')
+        if not refresh_token:
             raise AuthenticationFailed('Unauthenticated!')
 
-        try:
-            payload = jwt.decode(refreshToken, 'a-very-seCreT-kEy-WOW', algorithms=['HS256'])
-        except jwt.ExpiredSignatureError:
-            raise AuthenticationFailed('Unauthenticated!') 
-        #generating a an access toen with the valid refresh token 
-        id = payload['id']
-        payload2 = {
-            'id': id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
+        payload = decode_token(refresh_token)
+        #generating a an access token with the valid refresh token 
+        user_id = payload['id']
+        payload = {
+            'id': user_id,
         }       
-        AccessToken = jwt.encode(payload2, 'a-very-seCreT-kEy-WOW', algorithm='HS256') 
+        access_token = create_token(payload, TOKEN_EXPIRATION_ACCESS, settings.SECRET_KEY)
         response = Response()
         response.data = {
-            'AccessToken': AccessToken,
-            
+            'AccessToken': access_token,
         }
         return response
