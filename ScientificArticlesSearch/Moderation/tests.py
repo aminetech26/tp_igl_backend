09c95976e0370ff6b3911ddffd798ce9ca5f05f4
Benmachiche Khaled <lk_benmachiche@esi.dz>
2024-02-02 19:51:15 +0100

    unit test moderation.

--- ScientificArticlesSearch/Moderation/tests.py
+++ ScientificArticlesSearch/Moderation/tests.py
@@ -1,3 +1,109 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from Authentication.models import User
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+
+class ModerationApiTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.moderateur = User.objects.create(
+            first_name="Khaled",
+            last_name="BENMACHICHE",
+            username="khaledbenmachiche",
+            password="secure-password",
+            email="lk_benmachiche@esi.dz",
+            user_type="Mod"
+        )
+        
+    def setUp(self) -> None:
+        self.client = APIClient()
+        
+    def test_create_moderator_successfully(self):
+        url = reverse('moderation-list')
+        data = {
+            'email':"xxxx@xxx.x",
+            'username':"xxxx",
+            'password':"xxxx",
+            'first_name':"xxxx",
+            'last_name':"xxxx",
+            "user_type":"Mod"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 2)
+        self.assertEqual(User.objects.get(id=2).username, 'xxxx')
+        self.assertEqual(User.objects.get(id=2).email, 'xxxx@xxx.x')
+        self.assertEqual(User.objects.get(id=2).first_name, 'xxxx')
+        self.assertEqual(User.objects.get(id=2).last_name, 'xxxx')
+        self.assertEqual(User.objects.get(id=2).user_type, 'Mod')
+    
+    def test_create_moderator_missing_fields(self):
+        url = reverse('moderation-list')
+        data = {
+            'email':"xxxx@xxx.x",
+            'username':"xxxx",
+            'first_name':"xxxx",
+            'last_name':"xxxx",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+    
+    def test_create_moderator_existing_username(self):
+        url = reverse('moderation-list')
+        data = {
+            "first_name":"Khaled",
+            "last_name":"BENMACHICHE",
+            "username":"khaledbenmachiche",
+            "password":"secure-password",
+            "email":"lk_benmachiche@esi.dz",
+            "user_type":"Mod",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+    
+    def test_update_moderator_successfully(self):
+        url = reverse('moderation-detail', args=[self.moderateur.id])
+        data = {
+            "first_name":"Khaled",
+            "last_name":"BENMACHICHE",
+            "username":"khaledbenmachiche",
+            "password":"secure-password",
+            "email":"lk_benmachiche@esi.dz"
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.get(id=self.moderateur.id).username, 'khaledbenmachiche')
+        self.assertEqual(User.objects.get(id=self.moderateur.id).email, 'lk_benmachiche@esi.dz')
+        self.assertEqual(User.objects.get(id=self.moderateur.id).first_name, 'Khaled')
+        self.assertEqual(User.objects.get(id=self.moderateur.id).last_name, 'BENMACHICHE')
+        
+    def test_update_moderator_not_found(self):
+        url = reverse('moderation-detail', args=[100])
+        data = {
+            "first_name":"Khaled",
+            "last_name":"BENMACHICHE",
+            "username":"khaledbenmachiche",
+            "password":"secure-password",
+            "email":"lk_benmachiche@esi.dz"
+        }
+        response = self.client.put(url, data)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    def test_delete_moderator_not_found(self):
+        url = reverse('moderation-detail', args=[100])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_delete_moderator_by_ids_successfully(self):
+        url = reverse('moderation-delete_by_ids')
+        data = {
+            "moderators_ids":[1]
+        }
+        response = self.client.delete(url, data)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(User.objects.count(), 0)
+            
--- ScientificArticlesSearch/ScientificArticlesSearch/settings.py
+++ ScientificArticlesSearch/ScientificArticlesSearch/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import decouple 
-import config
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -115,20 +113,21 @@ WSGI_APPLICATION = 'ScientificArticlesSearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django"
 
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv('MYSQL_DATABASE'),
-    'USER': os.getenv('MYSQL_USER'),
-    'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-    'HOST': config('DB_HOST', 'db'),
-    'PORT': config('DB_PORT', '3306'),
+    'NAME': os.getenv('DB_NAME'),
+    'USER': os.getenv('DB_USER'),
+    'PASSWORD': os.getenv('DB_PASSWORD'),
+    'HOST': os.getenv('DB_HOST'),
+    'PORT': os.getenv('DB_PORT'),
     'OPTIONS': {'sslmode': 'require'},
     'DISABLE_SERVER_SIDE_CURSORS': True,
   }
 }
+
 
 
 
