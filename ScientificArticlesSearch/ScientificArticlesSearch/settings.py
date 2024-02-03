5defeea71a299fcd20ce52fd2a391390613a88a8
aminetech26 <la_guerraiche@esi.dz>
2024-02-03 13:38:10 +0100

    docker files modifications

--- ScientificArticlesSearch/ScientificArticlesSearch/settings.py
+++ ScientificArticlesSearch/ScientificArticlesSearch/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import decouple 
+import config
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -113,17 +115,20 @@ WSGI_APPLICATION = 'ScientificArticlesSearch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+SECRET_KEY = config('SECRET_KEY')
+
+# Database
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': os.getenv('DB_NAME'),
-    'USER': os.getenv('DB_USER'),
-    'PASSWORD': os.getenv('DB_PASSWORD'),
-    'HOST': os.getenv('DB_HOST'),
-    'PORT': os.getenv('DB_PORT'),
-    'OPTIONS': {'sslmode': 'require'},
-    'DISABLE_SERVER_SIDE_CURSORS': True,
-  }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('MYSQL_DATABASE'),
+        'USER': config('MYSQL_USER'),
+        'PASSWORD': config('MYSQL_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),  # Use 'db' as default from .env
+        'PORT': config('DB_PORT', '3306'),  # Use '3306' as default from .env
+        'OPTIONS': {'sslmode': 'require'},
+        'DISABLE_SERVER_SIDE_CURSORS': True,
+    }
 }
 
 
